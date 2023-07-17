65c4e256e7222b71fa1832cb38f86dfaef3afe21
sejal2117 <121280940+sejal2117@users.noreply.github.com>
2023-07-17 11:27:53 +0530

    Add files via upload

---
+++ components/footer.php
@@ -0,0 +1,33 @@
+<footer class="footer">
+
+   <section class="grid">
+
+      <div class="box">
+         <img src="images/email2.jpg" alt="">
+         <h3>Our email</h3>
+         <a href="mailto:cafesunset@gmail.com">cafesunset@gmail.com</a>
+         
+      </div>
+
+      <div class="box">
+         <img src="images/clock.jpg" alt="">
+         <h3>Opening hours</h3>
+         <p>10 am to 10 pm</p>
+      </div>
+
+      <div class="box">
+         <img src="images/location1.jpg" alt="">
+         <h3>our address</h3>
+         <a href="#">FC Road,Shivajinagar,Pune</a>
+      </div>
+
+      <div class="box">
+         <img src="images/phone.jpg" alt="">
+         <h3>our number</h3>
+         <a href="tel:9403088550">9403088550</a>
+      </div>
+
+   </section>
+
+   
+</footer>
